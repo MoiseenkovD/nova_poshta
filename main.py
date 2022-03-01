a6367b97bcee69286838d07a352cc29aa262df6d
danilmoiseenkov <danil.moiseenkoff@gmail.com>
2022-03-01 17:50:16 +0200

    fixed bugs

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def command(command_name):
     return decorator
 
 
-np_df = pd.read_excel('np.xlsx')
+np_df = pd.read_excel('data/np.xlsx')
 
 regions = list(np_df.sort_values(by='Область')['Область'].unique())
 
