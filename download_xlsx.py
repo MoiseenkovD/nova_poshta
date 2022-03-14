4e5c4bf344542c7e28d40c804271a9d5b72d231b
danilmoiseenkov <danil.moiseenkoff@gmail.com>
2022-03-14 21:47:58 +0200

    fixed downloading

--- download_xlsx.py
+++ download_xlsx.py
@@ -8,14 +8,15 @@ url = 'https://novaposhta.ua/news/rubric/2/id/10171'
 r = requests.get(url)
 html = bs(r.content, 'html.parser')
 
-select = html.select('.text > p > a')
+select = html.select('.text > p a')
 
 local_file = f'schedule-{datetime.now().strftime("%Y-%m-%d")}.xlsx'
 
-for link in select:
-    xlsx_np = link.attrs['href']
-    data = requests.get(xlsx_np)
-    res = re.search('\d+', link.text)
-    if res.group(0) == datetime.now().strftime('%-d'):
-        with open(f'data/{local_file}', 'wb') as file:
-            file.write(data.content)
+link = select[0]
+
+xlsx_np = link.attrs['href']
+data = requests.get(xlsx_np)
+res = re.search('\d+', link.text)
+if res.group(0) == datetime.now().strftime('%-d'):
+    with open(f'data/{local_file}', 'wb') as file:
+        file.write(data.content)
